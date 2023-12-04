e5449827e57d5d4164e305bc0235947ba60e7823
peerreez <92093034+peerreez@users.noreply.github.com>
2023-12-04 10:25:37 +0100

    Create tortuga.py

---
+++ tortuga.py
@@ -0,0 +1,16 @@
+import turtle
+import colorsys
+
+tortuga = turtle.Turtle()
+ventana = turtle.Screen()
+ventana.bgcolor("black")
+tortuga.speed(0)
+
+for i in range(125):
+    hue = i/125.0
+    color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+    tortuga.pencolor(color)
+    tortuga.forward(i*2)
+    tortuga.right(120)
+
+ventana.exitonclick()
