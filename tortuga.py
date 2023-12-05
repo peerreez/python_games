5509e249e512b65ae94d8a9e88d0e6600c16a4e2
peerrrez <92093034+peerreez@users.noreply.github.com>
2023-12-05 10:23:12 +0100

    Update tortuga.py

--- tortuga.py
+++ tortuga.py
@@ -1,16 +1,20 @@
 import turtle
 import colorsys
 
-tortuga = turtle.Turtle()
-ventana = turtle.Screen()
-ventana.bgcolor("black")
-tortuga.speed(0)
+def draw_spiral():
+    tortuga = turtle.Turtle()
+    ventana = turtle.Screen()
+    ventana.bgcolor("black")
+    tortuga.speed(0)
 
-for i in range(125):
-    hue = i/125.0
-    color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-    tortuga.pencolor(color)
-    tortuga.forward(i*2)
-    tortuga.right(120)
+    for i in range(125):
+        hue = i / 125.0
+        color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+        tortuga.pencolor(color)
+        tortuga.forward(i * 2)
+        tortuga.right(120)
 
-ventana.exitonclick()
+    ventana.exitonclick()
+
+if __name__ == "__main__":
+    draw_spiral()
